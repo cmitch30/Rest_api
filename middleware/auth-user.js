e19872ba72b7eed2e8be5998d534f65636c71913
clark <clark_mitchelljr@hotmail.com>
2023-01-05 11:22:07 -0700

    update

--- middleware/auth-user.js
+++ middleware/auth-user.js
@@ -11,23 +11,15 @@ exports.authenticateUser = async (req, res, next) => {
   // If the user's credentials are available...
   if (credentials) {
     // Attempt to retrieve the user from the data store
-    // by their username (i.e. the user's "key"
-    // from the Authorization header).
     const user = await User.findOne({ where: { emailAddress: credentials.name } });
     if (user) {
       // If a user was successfully retrieved from the data store...
-      // Use the bcrypt npm package to compare the user's password
-      // (from the Authorization header) to the user's password
-      // that was retrieved from the data store.
       const authenticated = bcrypt.compareSync(
         credentials.pass,
         user.password
       );
       if (authenticated) {
         // If the passwords match...
-        // Store the retrieved user object on the request object
-        // so any middleware functions that follow this middleware function
-        // will have access to the user's information.
         console.log(`Authentication successful for email address: ${user.emailAddress}`);
 
         req.currentUser = user;
--- routes/course.js
+++ routes/course.js
@@ -2,48 +2,101 @@ const express = require("express");
 const router = express.Router();
 const { asyncHandler } = require("../middleware/asyncHandler");
 const { authenticateUser } = require("../middleware/auth-user");
-const { Course } = require('../models')
+const { Course, User } = require("../models");
 
-router.get('/', asyncHandler(async(req, res) => {
-    const courses = req.currentCourse
-    console.log(courses)
-    res.status(200).json({
-        title: courses.title,
-        description: courses.description,
-        time: courses.estimatedTime,
-        materials: courses.materialsNeeded,
-        userId: courses.userId
-    })
-}))
+router.get(
+  "/",
+  asyncHandler(async(req, res) => {
+    const course = await Course.findAll({
+      attributes: [
+        "id",
+        "title",
+        "description",
+        "estimatedTime",
+        "materialsNeeded",
+        "userId",
+      ],
+      include: [
+        {
+          model: User,
+          as: "user",
+          attributes: ["firstName", "lastName", "emailAddress"],
+        },
+      ],
+    });
+    res.status(200).json(course);
+  })
+);
 
-router.get('/:id', asyncHandler(async(req, res) => {
-}))
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const course = await Course.findByPk(req.params.id, {
+      attributes: [
+        "id",
+        "title",
+        "description",
+        "estimatedTime",
+        "materialsNeeded",
+        "userId",
+      ],
+    });
+    res.status(200).json(course)
+  })
+);
 
-router.post('/', authenticateUser, asyncHandler(async(req, res) => {
-          try {
-            await Course.create(req.body);
-            res.status(201).json({ message: "Course successfully created!" });
-            res.location("/");
-          } catch (error) {
-            if (
-              error.name === "SequelizeValidationError" ||
-              error.name === "SequelizeUniqueConstraintError"
-            ) {
-              const errors = error.errors.map((err) => err.message);
-              res.status(400).json({ errors });
-            } else {
-              throw error;
+router.post(
+  "/",
+  authenticateUser,
+  asyncHandler(async (req, res) => {
+    try {
+      await Course.create(req.body);
+      res.status(201).json({ message: "Course successfully created!" });
+      res.location("/");
+    } catch (error) {
+      if (
+        error.name === "SequelizeValidationError" ||
+        error.name === "SequelizeUniqueConstraintError"
+      ) {
+        const errors = error.errors.map((err) => err.message);
+        res.status(400).json({ errors });
+      } else {
+        throw error;
+      }
+    }
+  })
+);
+
+router.put(
+  "/:id", authenticateUser,
+  asyncHandler(async (req, res) => {
+    const course = await Course.findByPk(req.params.id)
+    console.log(course)
+    if (course) {
+        course.title = req.body.title
+        course.description = req.body.description
+        await Course.update({title: req.body.title, description:req.body.description}, {
+            where: {
+                id: req.params.id
             }
-          }
-}))
+        })
+        res.status(204).end()
+    } else {
+         res.status(400).json({error: error});
+    }
+  })
+);
 
-router.put('/:id', authenticateUser, asyncHandler(async(req, res) => {
+router.delete(
+  "/:id",
+  authenticateUser,
+  asyncHandler(async (req, res) => {
+    const course = await Course.findByPk(req.params.id)
+    if (course) {
+        course.destroy()
+        res.status(204).end()
+    }
+  })
+);
 
-}))
-
-router.delete('/:id', authenticateUser, asyncHandler(async(req, res) => {
-    
-}))
-
-
-module.exports = router
+module.exports = router;
